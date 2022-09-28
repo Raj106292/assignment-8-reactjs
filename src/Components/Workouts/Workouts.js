468728ef9eea5acbd97f37a982d037239875897f
Raj106292 <rajdas.106292@gmail.com>
2022-09-29 00:39:21 +0600

    data loaded

--- src/Components/Workouts/Workouts.js
+++ src/Components/Workouts/Workouts.js
@@ -1,11 +1,25 @@
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Workouts.css';
 
 const Workouts = () => {
+    const [workouts, setWorkouts] = useState([]);
+    
+    useEffect(() => {
+        fetch('items.json')
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error.message))
+    },[])
+
     return (
-        <div>
-            <h2><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Fitness Zone</h2>
+        <div className='container'>
+            <div>
+                <h2><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Fitness Zone</h2>
+                <div className='workouts-container'>Workouts</div>
+            </div>
+            <div className='profile-container'>Profile</div>
         </div>
     );
 };
