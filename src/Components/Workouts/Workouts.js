fdbd1acc027d3e3d01868f4a8bcad0358f1f60ea
Raj106292 <rajdas.106292@gmail.com>
2022-09-29 05:46:14 +0600

    question-answer done

--- src/Components/Workouts/Workouts.js
+++ src/Components/Workouts/Workouts.js
@@ -10,17 +10,17 @@ const Workouts = () => {
 
     useEffect(() => {
         fetch('items.json')
-        .then(resp => resp.json())
-        .then(data => setWorkouts(data))
-        .catch(error => console.log(error.message))
-    },[])
+            .then(resp => resp.json())
+            .then(data => setWorkouts(data))
+            .catch(error => console.log(error.message))
+    }, [])
 
     const handleAddToList = (id) => {
         const selected = workouts.find(workout => workout.id === id);
         setSelected(selected);
     }
 
-    const [selected , setSelected] = useState([]);
+    const [selected, setSelected] = useState([]);
 
     return (
         <div className='container'>
@@ -33,6 +33,27 @@ const Workouts = () => {
                             workout={workout}
                             handleAddToList={handleAddToList}></Workout>)
                     }
+                </div>
+                <div className='question-answer'>
+                    <h2><u>Question-Answer:</u></h2>
+                    <ol>
+                        <li className='answer'>
+                            <p><strong>How does React work?</strong></p>
+                            <p>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</p>
+                        </li>
+                        <li className='answer'>
+                            <p><strong>What is the differences between Props and State</strong></p>
+                            <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
+                        </li>
+                        <li className='answer'>
+                            <p><strong>What does useEffect do without load API through fetch?</strong></p>
+                            <p>Accepts a function that contains imperative, possibly effectful code.
+
+                                Mutations, subscriptions, timers, logging, and other side effects are not allowed inside the main body of a function component. Doing so will lead to confusing bugs and inconsistencies in the UI. The clean-up function runs before the component is removed from the UI to prevent memory leaks. Additionally, if a component renders multiple times, the previous effect is cleaned up before executing the next effect. In our example, this means a new subscription is created on every update. To avoid firing an effect on every update, refer to the next section.
+
+                            </p>
+                        </li>
+                    </ol>
                 </div>
             </div>
             <div className='profile-container'>
